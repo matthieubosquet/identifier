af536825c1f1162ba367da67ba545699a7ea2c5c
Matthieu Bosquet <matthieu@cognithive.com>
2022-04-24 20:49:30 +0100

    Add type definitions

---
+++ src/crypg.d.ts
@@ -0,0 +1,5 @@
+export function id(): string;
+export function namednode(baseUrl?: string): string;
+export function url(baseUrl?: string): string;
+export function urn(): string;
+export function uuid(): string;
